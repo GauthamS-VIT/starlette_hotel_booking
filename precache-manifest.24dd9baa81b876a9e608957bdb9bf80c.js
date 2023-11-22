baf2b7c5f8a9a61da54cc71da138c1b5149a3f4e
GauthamS-VIT <144220553+GauthamS-VIT@users.noreply.github.com>
2023-11-22 20:29:21 +0530

    Updates

---
+++ precache-manifest.24dd9baa81b876a9e608957bdb9bf80c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7f8ec194c3b93e44c8413c438aab4ca",
+    "url": "/starlette_hotel_booking/index.html"
+  },
+  {
+    "revision": "0ae08f815c7a5e23fd1f",
+    "url": "/starlette_hotel_booking/static/css/2.55d6cd83.chunk.css"
+  },
+  {
+    "revision": "077ceba6a2c603608c6b",
+    "url": "/starlette_hotel_booking/static/css/main.f6176518.chunk.css"
+  },
+  {
+    "revision": "0ae08f815c7a5e23fd1f",
+    "url": "/starlette_hotel_booking/static/js/2.9356f68f.chunk.js"
+  },
+  {
+    "revision": "99b115a7aa0eb23e5a22c32ca89d0fff",
+    "url": "/starlette_hotel_booking/static/js/2.9356f68f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80e43ddc1f9f29be159a",
+    "url": "/starlette_hotel_booking/static/js/3.5263d583.chunk.js"
+  },
+  {
+    "revision": "077ceba6a2c603608c6b",
+    "url": "/starlette_hotel_booking/static/js/main.d2eb1503.chunk.js"
+  },
+  {
+    "revision": "7f4ac351cc0bbc1b2a61",
+    "url": "/starlette_hotel_booking/static/js/runtime-main.d4bbf400.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/starlette_hotel_booking/static/media/logo.06e73328.svg"
+  }
+]);
